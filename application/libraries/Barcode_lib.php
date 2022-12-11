d2cce62b533249476867588267a9d23efb68aeae
ManhVT <manhvt89@gmail.com>
2022-12-11 20:35:51 +0700

    ok

--- application/libraries/Barcode_lib.php
+++ application/libraries/Barcode_lib.php
@@ -197,10 +197,10 @@ class Barcode_lib
 	{
 		//$barcode_config['barcode_width'] = 145;
 		$barcode_config['barcode_width'] = 145;
-		$display_table = "<div class='' with='120px'>";
+		$display_table = "<div class='' with='145px'>";
 		$display_table .= "<div style='width:120px; font-size:9px; padding-bottom: 5px;' align='center'>" . $this->manage_display_layout($barcode_config['barcode_first_row'], $item, $barcode_config) . "</div>";
 		$barcode = $this->generate_barcode($item, $barcode_config);
-		$display_table .= "<div style='width:120px; font-size:9px;' align='center'><img style='width:120px;' src='data:image/png;base64,$barcode' /></div>";
+		$display_table .= "<div style='width:100%; font-size:9px;' align='center'><img src='data:image/png;base64,$barcode' /></div>";
 		$display_table .= "<div style='width:120px; font-size:9px;' align='center'>" . $this->manage_display_layout($barcode_config['barcode_second_row'], $item, $barcode_config) . "</div>";
 		//$display_table .= "<tr><td align='center'>" . $this->manage_display_layout($barcode_config['barcode_third_row'], $item, $barcode_config) . "</td></tr>";
 		$display_table .= "</div>";
