7291431de89cf52c9517ecf600c9ceb409afc743
ManhVT <manhvt89@gmail.com>
2022-12-13 18:49:04 +0700

    ok

--- application/libraries/Barcode_lib.php
+++ application/libraries/Barcode_lib.php
@@ -197,7 +197,7 @@ class Barcode_lib
 	{
 		//$barcode_config['barcode_width'] = 145;
 		$barcode_config['barcode_width'] = 145;
-		$display_table = "<div class='' style='width:150px; outline: 1px dashed;border-spacing: 1px;'>";
+		$display_table = "<div class='' style='width:150px; '>";
 		$display_table .= "<div style='width:155px; font-size:9px; padding-bottom: 5px;' align='center'>" . $this->manage_display_layout($barcode_config['barcode_first_row'], $item, $barcode_config) . "</div>";
 		$barcode = $this->generate_barcode($item, $barcode_config);
 		$display_table .= "<div style='width:100%; font-size:9px;' align='left'><img src='data:image/png;base64,$barcode' /></div>";
