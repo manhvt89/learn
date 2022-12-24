c90a95a49b834043f1877eabe90253ae47702a4d
ManhVT <manhvt89@gmail.com>
2022-12-24 15:51:33 +0700

    ok

--- application/controllers/Reports.php
+++ application/controllers/Reports.php
@@ -1051,7 +1051,8 @@ class Reports extends Secure_Controller
 				'payment_type' => $row['payment_type'],
 				'comment' => $row['comment'],
 				'edit' => anchor('sales/edit/'.$row['sale_id'], '<span class="glyphicon glyphicon-edit"></span>',
-					array('class' => 'modal-dlg print_hide', 'data-btn-delete' => $this->lang->line('common_delete'), 'data-btn-submit' => $this->lang->line('common_submit'), 'title' => $this->lang->line('sales_update'))
+					//array('class' => 'modal-dlg print_hide', 'data-btn-delete' => $this->lang->line('common_delete'), 'data-btn-submit' => $this->lang->line('common_submit'), 'title' => $this->lang->line('sales_update'))
+                    array('class' => 'modal-dlg print_hide', 'data-btn-submit' => $this->lang->line('common_submit'), 'title' => $this->lang->line('sales_update'))
 				)
 			));
             if($report_data['details'][$key] == null)
