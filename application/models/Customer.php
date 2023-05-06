70e0e2b2fb8ada630904edc635b3a244008f2565
ManhVT <manhvt89@gmail.com>
2023-05-06 08:42:07 +0700

    o

--- application/models/Customer.php
+++ application/models/Customer.php
@@ -195,9 +195,14 @@ class Customer extends Person
 		$this->db->group_start();		
 			//$this->db->like('first_name', $search);
 			//$this->db->or_like('last_name', $search); 
+			
+		if(ctype_digit($search))
+		{
+			$this->db->like('phone_number',$search);
+		} else {
 			$this->db->like('CONCAT(last_name, " ", first_name)', $search);
-			$this->db->or_like('phone_number',$search);
 			$this->db->or_like('account_number', $search);
+		}
 		$this->db->group_end();
 		$this->db->where('deleted', 0);
 		$this->db->order_by('last_name', 'asc');
