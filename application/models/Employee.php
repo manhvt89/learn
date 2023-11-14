dd9addbcf1019da2628a0b94531a3f3874be06b7
ManhVT <manhvt89@gmail.com>
2023-11-14 21:28:59 +0700

    o

--- application/models/Employee.php
+++ application/models/Employee.php
@@ -86,13 +86,13 @@ class Employee extends Person
 	/*
 	Inserts or updates an employee
 	*/
-	public function save_employee(&$person_data, &$employee_data, &$employee_data, $employee_data = FALSE)
+	public function save_employee(&$person_data, &$employee_data, &$employee_data, $employee_id = FALSE)
 	{
 		$success = FALSE;
 		//var_dump($person_data);
 		//var_dump($employee_data);
 		//var_dump($employee_data);
-		var_dump($employee_data);//die();
+		var_dump($employee_id);//die();
 		//Run these queries as a transaction, we want to make sure we do all or nothing
 		$this->db->trans_start();
 
