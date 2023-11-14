65434eb65b3b8c83975e3dd79b0ce62a32c7f1f4
ManhVT <manhvt89@gmail.com>
2023-11-14 22:24:19 +0700

    o

--- application/models/Employee.php
+++ application/models/Employee.php
@@ -108,6 +108,8 @@ class Employee extends Person
 				//echo 'manhvt2';
 				$employee_data['person_id'] = $employee_id = $person_data['person_id'];
 				$success = $this->db->insert('employees1', $employee_data);
+				var_dump($employee_data);
+				die();
 			}
 			else
 			{
